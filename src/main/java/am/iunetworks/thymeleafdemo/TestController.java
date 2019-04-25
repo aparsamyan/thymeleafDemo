ab1eaa3b8527c433875aa06c6c7a3acd527f9f92
Aida Parsamyan <aida.parsamyan@iunetworks.local>
2019-04-25 15:21:53 +0400

    test_project

---
+++ src/main/java/am/iunetworks/thymeleafdemo/TestController.java
@@ -0,0 +1,17 @@
+package am.iunetworks.thymeleafdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class TestController {
+
+
+    @GetMapping(value = "/test")
+    public String getTestPage() {
+
+        return "test";
+    }
+}
